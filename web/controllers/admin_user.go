03aefee39fa196aaac0854876c985d8d07a98c1e
陈昭良 <czl2106082@163.com>
2019-10-24 16:01:16 +0800

    做好用户界面

--- web/controllers/admin_user.go
+++ web/controllers/admin_user.go
@@ -2,11 +2,13 @@ package controllers
 
 import (
 	"Lottery-go/comm"
+	"Lottery-go/models"
 	"Lottery-go/services"
 	"Lottery-go/web/viewmodels"
 	"fmt"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
+	"time"
 )
 
 type AdminUserController struct {
@@ -68,7 +70,47 @@ func (c* AdminUserController) PostSave() mvc.Result {
 			Text: fmt.Sprintf("ReadForm转换异常, err=%s", err),
 		}
 	}
-	return mvc.View{
+	t, err := comm.ParseTime(data.BlackTime)
+	if err != nil {
+		return mvc.Response{
+			Text: fmt.Sprintf("黑名单到期时间格式有误, err=%s", err),
+		}
+	}
+	user := models.User{
+		UserName:   data.UserName,
+		BlackTime:  int(t.Unix()),
+		RealName:   data.RealName,
+		Mobile:     data.Mobile,
+		Address:    data.Address,
+		SysCreated: int(time.Now().Unix()),
+		SysUpdated: int(time.Now().Unix()),
+		SysIp:      comm.ClientIP(c.Ctx.Request()),
+	}
+	err = c.ServiceUser.Create(&user)
+	if err != nil {
+		return mvc.Response{
+			Text: fmt.Sprintf("添加用户失败, err=%s", err),
+		}
+	}
+	return mvc.Response {
+		Path: "/admin/user",
+	}
+}
 
+func (c* AdminUserController) GetBlack() mvc.Result {
+	id := c.Ctx.URLParamIntDefault("id", 0)
+	t := c.Ctx.URLParamIntDefault("time",0)
+	if id > 0 {
+		user := c.ServiceUser.Get(id)
+		user.BlackTime = int(time.Now().Unix()) + t * 86400
+		err := c.ServiceUser.Update(user, []string{"black_time", "sys_update"})
+		if err != nil {
+			return mvc.Response{
+				Text: fmt.Sprintf("设置用户黑名单失败, err=%s", err),
+			}
+		}
+	}
+	return mvc.Response{
+		Path: "/admin/user",
 	}
 }
